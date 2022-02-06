66b9c2ed7f82aa1a6f1b0e692bd8bdf4ab15be80
AMALU SAJEEV <amalu.sajeev.me@gmail.com>
2022-02-06 17:40:11 +0530

    Update ListMember.js

--- src/Component/ListMember/ListMember.js
+++ src/Component/ListMember/ListMember.js
@@ -4,18 +4,15 @@ function ListMember() {
     const[ user, setUser ] = useState([])
     useEffect(() => {
         (async function () {
-            const headersList = {
-                'Accept': '*/*',
-                'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
-                'content-type': 'application/x-www-form-urlencoded'
-            }
+//             const headersList = {
+//                 'Accept': '*/*',
+//                 'User-Agent': 'Thunder Client (https://www.thunderclient.com)',
+//                 'content-type': 'application/x-www-form-urlencoded'
+//             }
             // application/x-www-form-urlencoded
             // application/json; charset=utf-8
             const loginRequest = new URL("https://bug-free.herokuapp.com/api/members/all");
-            await fetch(loginRequest, {
-              method: "GET",
-              headers: headersList
-            }).then(response => {
+            await fetch(loginRequest).then(response => {
                 return response.json();
             }).then(data => {
                   console.log(data);
